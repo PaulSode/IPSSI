ab4e069ae049e14fb59de283dcd7efff1e0ac3ac
NihachiOokami <paulsode05@gmail.com>
2025-02-17 15:14:45 +0100

    users map

--- app-tp1/src/services/components/cards/cards.jsx
+++ app-tp1/src/services/components/cards/cards.jsx
@@ -1,12 +1,10 @@
 ﻿import style from "./cards.module.css"
 
-function Cards(){
-    return <>
-        <h1 className={style.titre}>Coucou</h1>
-        <p className={style.paragraphe}>C'est moi Mickey</p>
-        <p className={style.paragraphe}>Non en vrai c'est pas moi désolé</p>
-    </>
-
-}
-
+const Cards = ({prenom, nom}) => {
+        return <>
+            <h1 className={style.titre}>Coucou</h1>
+            <p className={style.paragraphe}>C'est moi {prenom} {nom}</p>
+            <p className={style.paragraphe}>Non en vrai c'est pas moi désolé</p>
+        </>
+    };
 export default Cards;
