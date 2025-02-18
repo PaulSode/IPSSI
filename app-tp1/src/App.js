cbe5c7d75b9ed9303a08922a84acb8e9c0fcadc7
NihachiOokami <paulsode05@gmail.com>
2025-02-18 11:39:59 +0100

    travail mardi matin

--- app-tp1/src/App.js
+++ app-tp1/src/App.js
@@ -1,48 +1,39 @@
 import './App.css';
-import {Cards, PostCards} from "./services/components/cards/cards";
 import Header from "./services/components/Header/header";
 import Footer from "./services/components/Footer/footer";
-import Bouton from "./services/components/button/button";
-import {tasks} from "./data/users";
+
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+
+import Accueil from "./pages/Accueil";
+import Details from "./pages/Details";
+import ErrorPage from "./pages/404/ErrorPage";
+import RoutePosts from "./services/Router/RoutePosts";
+import {PostCards} from "./services/components/cards/cards";
+import PostsAPI from "./services/postsAPI";
 import {useEffect, useState} from "react";
 
 function App() {
 
-  const [count, setCount] = useState(0);
+
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(response => response.json())
-        .then(post => setPosts(post));
+    PostsAPI.getPosts().then(response => {setPosts(response.data);})
   }, []);
+  //{posts.map(post => <PostCards key={post.id} {...post}></PostCards>)}
 
-  console.log(posts);
 
-  useEffect(() => {}, [])
-
-  const handleIncrement = () => {
-    setCount(count + 1);
-  };
-
-  const handleDecrement = () => {
-    setCount(count - 1);
-  };
 
   return <>
     <Header></Header>
 
-    <h1>{count}</h1>
-    <Bouton label="Increment" onClick={handleIncrement}></Bouton>
-    <Bouton label="Decrement" onClick={handleDecrement}></Bouton>
+    <Router>
+      <nav>
+        <Link to="/">Accueil</Link> | <Link to="/details">Details</Link>
+      </nav>
+      <RoutePosts></RoutePosts>
+    </Router>)
 
-    <h2>Toutes les tâches</h2>
-    {tasks.map(user => <Cards key={user.id} {...user}></Cards>)}
-    <br></br>
-    <h2>Tâches finies</h2>
-    {tasks.filter(user => user.fini === "Oui").map(user => <Cards key={user.id} {...user}></Cards>)}
-
-    {posts.map(post => <PostCards key={post.id} {...post}></PostCards>)}
 
     <Footer></Footer>
   </>
---
+++ app-tp1/src/pages/404/ErrorPage.jsx
@@ -0,0 +1,11 @@
+﻿import {BrowserRouter as Router, Link} from "react-router-dom";
+import RoutePosts from "../../services/Router/RoutePosts";
+
+const ErrorPage = () => {
+
+    return <><h1>Error</h1>
+
+    </>
+}
+
+export default ErrorPage;
