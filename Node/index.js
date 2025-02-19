39f45e011365904010a4ef87a8b3ffcf8a768cfd
NihachiOokami <paulsode05@gmail.com>
2025-02-19 11:45:56 +0100

    fixed node

---
+++ Node/index.js
@@ -0,0 +1,12 @@
+﻿import http from 'http';
+import dotenv from 'dotenv'
+
+const server = http.createServer()
+
+dotenv.config()
+const port = process.dotenv.port
+
+
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+})
